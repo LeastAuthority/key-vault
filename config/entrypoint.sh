4a33d3d4a3863fb658ef6d4e28ce3575a601aebb
systemblox <systemblox@blox.io>
2020-06-10 17:38:45 +0300

    Setup init script

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -2,22 +2,3 @@
 set -e
 
 vault server -config=/vault/config/vault-config.json
-
-# Init Vault
-echo "Init vault..."
-vault operator init  &> /vault/keys.txt
-
-if ! grep -q "Vault is already initialized" /vault/keys.txt; then
-
-# Unseal Vault
-echo "Unseal vault..."
-vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 1:' /vault/keys.txt | awk '{print $NF}')
-vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 2:' /vault/keys.txt | awk '{print $NF}')
-vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 3:' /vault/keys.txt | awk '{print $NF}')
-vault login $(grep 'Initial Root Token:' /vault/keys.txt | awk '{print $NF}') > /vault/token.txt
-echo "###Please save the token information.###"
-cat /vault/token.txt
-
-else
-    echo "Vault is already initialized!"
-fi
