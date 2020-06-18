6abe0fa0d2c37391e9b18148d370f7afd50f1448
systemblox <systemblox@blox.io>
2020-06-18 19:24:27 +0300

    Setup vault unseal script

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-vault server -config=/vault/config/vault-config.json > /dev/null
+vault server -config=/vault/config/vault-config.json > /dev/null 2&1
+sleep 5
+vault operator init -status
+if [[ $? -eq 2 ]]; then
+ vault operator init -key-shares=1 -key-threshold=1 -recovery-shares=1 -recovery-threshold=1 -format=json > /tmp/vault.init 2>&1
+  cat /tmp/vault.init | jq -r '.root_token' > /keys/vault.root.token && chmod 0400 /keys/vault.root.token 
+  cat /tmp/vault.init | jq -r '.unseal_keys_b64[]' > /keys/vault.unseal.token && chmod 0400 /keys/vault.unseal.token
+  rm -f /tmp/vault.*
+fi 
+
+vault status 
+if [[ $? -eq 2 ]]; then 
+  vault operator unseal $(cat /keys/vault.unseal.token ) >/dev/null 2>&1
+  vault login $(cat /keys/vault.root.token ) >/dev/null 2>&1
+fi 
+
+sleep 356000d
