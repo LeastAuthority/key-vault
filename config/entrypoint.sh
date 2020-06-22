bd5902388c323182a2d1d0bee3531518fe983125
systemblox <systemblox@blox.io>
2020-06-22 13:26:47 +0300

    Reconfigured entrypoint

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -21,7 +21,7 @@ fi
 # Upgrade Ethereum 2.0 Signing Plugin
 #vault login $(cat /data/keys/vault.root.token)
 export SHASUM256=$(sha256sum "/vault/plugins/ethsign" | cut -d' ' -f1)
-vault write /sys/plugins/catalog/secret/ethsign sha_256=${SHASUM256} command=ethsign
+vault write /sys/plugins/catalog/secret/ethsign sha_256=${SHASUM256} command=ethsign > /dev/null 2>&1
 vault secrets enable -path=ethereum -description="Eth Signing Wallet" -plugin-name=ethsign plugin > /dev/null 2>&1
 curl --header "X-Vault-Token: $(cat /data/keys/vault.root.token)" --request PUT --data '{"plugin": "ethsign"}'  http://127.0.0.1:8200/v1/sys/plugins/reload/backend
 
