73c72489795b60e4dbf359560e6b8659fa1998df
Alon Muroch <alonmuroch@gmail.com>
2020-07-29 12:47:18 +0300

    KV0.2 wallet path

--- backend/path_wallets.go
+++ backend/path_wallets.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	vault "github.com/bloxapp/KeyVault"
-	"github.com/bloxapp/KeyVault/core"
 	store "github.com/bloxapp/KeyVault/stores/hashicorp"
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
@@ -14,21 +13,11 @@ import (
 func walletsPaths(b *backend) []*framework.Path {
 	return []*framework.Path{
 		&framework.Path{
-			Pattern: "wallets/?",
-			Callbacks: map[logical.Operation]framework.OperationFunc{
-				logical.ListOperation: b.pathWalletsList,
-			},
-			HelpSynopsis: "List all the Ethereum 2.0 wallets at a path",
-			HelpDescription: `
-			All the Ethereum 2.0 wallets will be listed.
-			`,
-		},
-		&framework.Path{
-			Pattern:         "wallets/" + framework.GenericNameRegex("wallet_name"),
+			Pattern:         "wallet/",
 			HelpSynopsis:    "Create an Ethereum 2.0 wallet",
 			HelpDescription: ``,
 			Fields: map[string]*framework.FieldSchema{
-				"wallet_name": &framework.FieldSchema{Type: framework.TypeString},
+
 			},
 			ExistenceCheck: b.pathExistenceCheck,
 			Callbacks: map[logical.Operation]framework.OperationFunc{
@@ -39,54 +28,31 @@ func walletsPaths(b *backend) []*framework.Path {
 }
 
 func (b *backend) pathWalletCreate(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-	walletName := data.Get("wallet_name").(string)
 	storage := store.NewHashicorpVaultStore(req.Storage, ctx)
-	options := vault.PortfolioOptions{}
+	options := &vault.KeyVaultOptions{}
 	options.SetStorage(storage)
-	portfolio, err := vault.OpenKeyVault(&options)
-	if err != nil {
-		switch e := err.(type) {
-		// TODO move to portfolio create path
-		case *vault.NotExistError:
-			portfolio, err = vault.NewKeyVault(&options)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to create key vault")
-			}
-		default:
-			return nil, errors.Wrap(e, "failed to open key vault")
-		}
+
+	// check a wallet doesn't exist
+	existingKv, err := vault.OpenKeyVault(options)
+	if err == nil || existingKv != nil {
+		return nil, errors.New("KeyVault wallet already exists")
 	}
 
-	wallet, err := portfolio.CreateWallet(walletName)
+
+	// create new KeyVault (which creates new wallet)
+	kv, err := vault.NewKeyVault(options)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to create new wallet")
+		return nil, errors.Wrap(err, "failed to create key vault")
+	}
+
+	wallet, err := kv.Wallet()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create wallet")
 	}
 
 	return &logical.Response{
 		Data: map[string]interface{}{
 			"wallet": wallet,
-		},
-	}, nil
-}
-
-func (b *backend) pathWalletsList(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-	storage := store.NewHashicorpVaultStore(req.Storage, ctx)
-	options := vault.PortfolioOptions{}
-	options.SetStorage(storage)
-
-	portfolio, err := vault.OpenKeyVault(&options)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to open key vault")
-	}
-
-	var wallets []core.Wallet
-	for w := range portfolio.Wallets() {
-		wallets = append(wallets, w)
-	}
-
-	return &logical.Response{
-		Data: map[string]interface{}{
-			"wallets": wallets,
 		},
 	}, nil
 }
--- backend/path_wallets_test.go
+++ backend/path_wallets_test.go
@@ -36,58 +36,17 @@ func TestWalletCreate(t *testing.T) {
 	var storage logical.Storage
 
 	t.Run("Successfully Create Wallet", func(t *testing.T) {
-		req := logical.TestRequest(t, logical.CreateOperation, "wallets/wallet1")
+		req := logical.TestRequest(t, logical.CreateOperation, "wallet/")
 		storage = req.Storage
 		res, err := b.HandleRequest(context.Background(), req)
 		require.NoError(t, err)
 		require.NotNil(t, res.Data["wallet"])
 	})
 
-	t.Run("Create Wallet with empty name", func(t *testing.T) {
-		req := logical.TestRequest(t, logical.CreateOperation, "wallets/ ")
-		_, err := b.HandleRequest(context.Background(), req)
-		require.EqualError(t, err, "unsupported path")
-	})
-
-	t.Run("Create Wallet with existing name", func(t *testing.T) {
-		req := logical.TestRequest(t, logical.CreateOperation, "wallets/wallet1")
+	t.Run("Create Wallet when one already exists", func(t *testing.T) {
+		req := logical.TestRequest(t, logical.CreateOperation, "wallet/")
 		req.Storage = storage
 		_, err := b.HandleRequest(context.Background(), req)
-		require.EqualError(t, err, "failed to create new wallet: wallet \"wallet1\" already exists")
-	})
-
-	// TODO
-	//t.Run("Create Wallet with too long name (more than 128 characters)", func(t *testing.T) {
-	//
-	//})
-
-	//data = map[string]interface{}{
-	//	"walletName":  "wallet1",
-	//	"accountName": "account1",
-	//}
-	//req.Data = data
-}
-
-func TestWalletsList(t *testing.T) {
-	b, _ := getBackend(t)
-	var storage logical.Storage
-
-	t.Run("Successfully List Wallets", func(t *testing.T) {
-		req := logical.TestRequest(t, logical.CreateOperation, "wallets/wallet1")
-		storage = req.Storage
-		_, err := b.HandleRequest(context.Background(), req)
-		require.NoError(t, err)
-
-		req = logical.TestRequest(t, logical.ListOperation, "wallets/")
-		req.Storage = storage
-		res, err := b.HandleRequest(context.Background(), req)
-		require.NoError(t, err)
-		require.NotNil(t, res.Data["wallets"])
-	})
-
-	t.Run("List Wallets in non existing portfolio", func(t *testing.T) {
-		req := logical.TestRequest(t, logical.ListOperation, "wallets/")
-		_, err := b.HandleRequest(context.Background(), req)
-		require.EqualError(t, err, "failed to open key vault: key vault not found")
+		require.EqualError(t, err, "KeyVault wallet already exists")
 	})
 }
