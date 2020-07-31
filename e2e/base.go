6844da1c10e084785255084b269e0dd0f348188f
Alon Muroch <alonmuroch@gmail.com>
2020-07-31 17:38:02 +0300

    complete base setup working

--- e2e/base.go
+++ e2e/base.go
@@ -1,14 +1,35 @@
 package e2e
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-func cleanup() error {
-	const containerName = "vault-plugin-secrets-eth20_vault_1"
+const (
+	pluginContainerName         = "vault-plugin-secrets-eth20_vault_1"
+	logSignalingPluginInstalled = "core: successfully reloaded plugin: plugin=ethsign path=ethereum/"
+	dataDirSuffix				= "/data/"
+	rootTokenSuffix             = dataDirSuffix + "keys/vault.root.token"
+)
+
+func Cleanup(workingDir string) error {
+	// Cleanup data dir
+	dataDir := fmt.Sprintf("%s%s", workingDir, dataDirSuffix)
+	_, err := os.Stat(dataDir)
+	if !os.IsNotExist(err) {
+		err := os.RemoveAll(dataDir)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Cleanup: deleted data dir\n")
+	}
+
+
 	// check if running
 	listCmd := exec.Command("docker", "container", "ps", "-ls")
 	byts,err := listCmd.CombinedOutput()
@@ -16,50 +37,95 @@ func cleanup() error {
 		return err
 	}
 	containerList := string(byts)
-	if !strings.Contains(containerList, containerName) {
+	if !strings.Contains(containerList, pluginContainerName) {
+		fmt.Printf("Cleanup: NO previous container\n")
 		return nil
 	}
 
 	// kill process
-	killCmd := exec.Command("docker", "kill",containerName)
+	killCmd := exec.Command("docker", "kill", pluginContainerName)
 	err = killCmd.Run()
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Cleanup: killing previous container\n")
 
 	// remove container and volumes
-	removeCmd := exec.Command("docker", "rm",containerName, "--volumes")
+	removeCmd := exec.Command("docker", "rm", pluginContainerName, "--volumes")
 	err = removeCmd.Run()
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Cleanup: deleting previous container\n")
 
 	return nil
 }
 
-func SetupE2EEnv() error {
-	// step 1 - cleanup
-	err := cleanup()
-	if err != nil {
-		return err
-	}
-	fmt.Printf("cleanup done\n")
+func pluginRunning(closer io.ReadCloser) <- chan bool {
+	ret := make(chan bool)
 
-	// step 2 - run docker compose
+	scanner := bufio.NewScanner(closer)
+
+	go func() {
+		for scanner.Scan() {
+			newLine := scanner.Text()
+			fmt.Println(newLine)
+
+			if strings.Contains(newLine, logSignalingPluginInstalled) {
+				ret <- true
+				close(ret)
+				return
+			}
+		}
+	}()
+
+	return ret
+}
+
+func rootAccessToken(workingDir string) (string, error) {
+	content, err := ioutil.ReadFile(fmt.Sprintf("%s%s", workingDir, rootTokenSuffix))
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func SetupE2EEnv() error {
 	workingDir, err := os.Getwd()
 	if err != nil {
 		return err
 	}
 	fmt.Printf("working dir: %s\n", workingDir)
 
-	cmd := exec.Command("docker-compose", "up","vault")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err = cmd.Run()
+	// step 1 - Cleanup
+	err = Cleanup(workingDir)
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Cleanup done\n")
+
+	// step 2 - run docker compose
+	cmd := exec.Command("docker-compose", "up","vault")
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// step 3 - wait for plugin to be active
+	<- pluginRunning(pipe)
+	fmt.Printf("Plugin installed and running\n")
+
+	// step 4 - get root access token
+	token, err := rootAccessToken(workingDir)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("root token: %s\n", token)
 
 	return nil
 }
