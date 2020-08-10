27bdb91377eaea9c2b33463302685b8b27e8b140
begmaroman <r.behma@svitla.com>
2020-08-10 12:23:07 +0300

    adjusting tests

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-if [[ $* == *--test* ]]
-then
-    echo "TESTS"
-else
-    mkdir /data
-    vault server -config=/vault/config/vault-config.json -log-level=debug > /data/logs 2&1
-    sleep 5
-    if [ "$UNSEAL" = "true" ]; then
-      /bin/sh /vault/config/vault-init.sh
-      /bin/sh /vault/config/vault-unseal.sh
-      /bin/sh /vault/config/vault-plugin.sh
+mkdir /data
+vault server -config=/vault/config/vault-config.json -log-level=debug > /data/logs 2&1
+sleep 5
+if [ "$UNSEAL" = "true" ]; then
+  /bin/sh /vault/config/vault-init.sh
+  /bin/sh /vault/config/vault-unseal.sh
+  /bin/sh /vault/config/vault-plugin.sh
 
-    fi
-
-    sleep 356000d
 fi
 
+sleep 356000d
